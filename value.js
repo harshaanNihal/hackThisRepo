6b76f2f79100283b856d1bf884f7b67e9604e5a8
Ashwini Gupta <80703231+Ashwinigpt@users.noreply.github.com>
2021-10-27 08:24:35 +0530

    Create value.js (#69)

---
+++ value.js
@@ -0,0 +1,16 @@
+
+
+let num1 = 21;
+let num2 = 32;
+
+
+
+let numA = Number(prompt("Enter first number!"));
+let numB = Number(prompt("Enter second number!"));
+
+alert(numA + numB);
+
+alert(numA - numB);
+
+console.log(numA + numB);
+console.log(numA - numB);
