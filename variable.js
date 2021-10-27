ef311d61458cf84abcf7d273944d3c39d560c2bd
Ashwini Gupta <80703231+Ashwinigpt@users.noreply.github.com>
2021-10-27 08:24:42 +0530

    Create variable.js (#68)

---
+++ variable.js
@@ -0,0 +1,31 @@
+
+
+let num1 = 21;
+let num2 = 32;
+
+
+let language = "JavaScript";
+alert(`I am learning ${language}`);
+alert(`The value of num1 is ${num1} and the value of num2 is ${num2}`);
+alert(`The sum of ${num1} and ${num2} is ${num1} + ${num2}`);
+
+
+let userName = prompt("What is your name?");
+let userProfession = prompt("What is your profession?");
+let isAdult = confirm("Are you an adult?");
+alert(`I am ${userName} a ${userProfession}`);
+console.log(`I am ${userName} a ${userProfession}`);
+alert(`Username: ${userName}
+ Adult: ${isAdult}
+Profession: ${userProfession}`);
+
+
+let numA = Number(prompt("Enter first number!"));
+let numB = Number(prompt("Enter second number!"));
+
+alert(numA + numB);
+
+alert(numA - numB);
+
+console.log(numA + numB);
+console.log(numA - numB);
