35d8553bf39fb1628b64d1d54d13a09cc9b06c66
klassynihal <56109924+klassynihal@users.noreply.github.com>
2021-10-31 19:49:44 +0530

    Update unique.js (#94)

--- unique.js
+++ unique.js
@@ -3,5 +3,4 @@ const unique = arr => arr.filter((v,i,a)=>a.indexOf(v) == i);
 //OR
 
 const data = [1, 2, 3, 1, 2, 4]
-const values = new Set(data);
-const uniqueValues = [...values]; 
+const uniqueValues = [... new Set(data)]; 
