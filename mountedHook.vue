4224ba2333d82a2430d0786279b20458c338c93e
XzehanX <116910829+XzehanX@users.noreply.github.com>
2022-10-28 23:23:21 +0530

    Create mountedHook.vue

---
+++ mountedHook.vue
@@ -0,0 +1,12 @@
+<template>
+  <div ref="example-element">Example component.</div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    console.log(`At this point, vm.$el has been created and el has been replaced.`);
+    console.log(this.$el.textContent) // Example component.
+  }
+}
+</script>
