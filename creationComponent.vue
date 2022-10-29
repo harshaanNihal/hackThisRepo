ee59a09078691d3dde02a2a79c56485f43b2578f
XzehanX <116910829+XzehanX@users.noreply.github.com>
2022-10-29 23:28:26 +0530

    Create creationComponent.vue (#111)

---
+++ creationComponent.vue
@@ -0,0 +1,24 @@
+<template>
+  <div ref="example-element">{{ propertyComputed }}</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      property: 'Example property.'
+    }
+  },
+
+  computed: {
+    propertyComputed() {
+      return this.property
+    }
+  },
+
+  created() {
+    console.log('At this point, this.property is now reactive and propertyComputed will update.')
+    this.property = 'Example property updated.'
+  }
+}
+</script>
