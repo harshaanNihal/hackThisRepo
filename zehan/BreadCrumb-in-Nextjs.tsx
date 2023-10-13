801477e61b18ae5736f804bef891b7f23a7580f6
Zehan Khan <73664886+zehan12@users.noreply.github.com>
2023-10-13 15:51:43 +0530

    feat: :sparkles: add breadcrumb in Next js with tailwind (#119)

---
+++ zehan/BreadCrumb-in-Nextjs.tsx
@@ -0,0 +1,52 @@
+'use client';
+import React, { useEffect } from 'react';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import { BiChevronRight } from "react-icons/bi";
+import { AiFillHome } from "react-icons/ai";
+
+const get_breadcrumbs = function (url: string) {
+    var rtn = [{ name: "Home", url: "/" }],
+        acc = "", // accumulative url
+        arr = url.substring(1).split("/");
+
+    for (let i = 0; i < arr.length; i++) {
+        acc = i != arr.length - 1 ? acc + "/" + arr[i] : "";
+        rtn[i + 1] = {
+            name: arr[i].charAt(0).toUpperCase() + arr[i].slice(1),
+            url: acc,
+        };
+    }
+    return rtn;
+};
+
+interface BreadcrumbsProps { }
+
+const Breadcrumbs:React.FC<BreadcrumbProps> = ( props ) => {
+    const path = usePathname();
+    const routes = get_breadcrumbs(path);
+    return (
+        <nav className="flex" aria-label="Breadcrumb">
+            <ol role="list" className="flex items-center space-x-4">
+                {routes.map((path, index) => (
+                    <li key={path.name}>
+                        <div className="flex items-center">
+                            {
+                                path.name === "Home" ? "" :
+                                    <BiChevronRight className="h-5 w-5 flex-shrink-0 text-gray-400" />
+                            }
+                            <Link
+                                href={path.name === "Home" ? "/" : "/" + path.name.toLowerCase()}
+                                className={`ml-4 text-sm font-medium hover:text-gray-700 ${routes.length - 1 !== index ? "text-gray-400" : "text-black"}`}
+                            >
+                                {path.name === "Home" ? <AiFillHome className="h-5 w-5 flex-shrink-0" /> : path.name.toLowerCase()}
+                            </Link>
+                        </div>
+                    </li>
+                ))}
+            </ol>
+        </nav>
+    )
+};
+
+export default Breadcrumbs;
