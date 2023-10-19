beb183d60ea51ab578da466e1890373cf3560e60
Harshaan Nihal Khan <harshaan.n.khan@gmail.com>
2023-10-19 07:49:23 +0700

    Update styles.js

--- Todolist/styles.js
+++ Todolist/styles.js
@@ -3,16 +3,19 @@ const itemList = document.querySelector(".ul");
 const complt = document.querySelector(".complt");
 const active = document.querySelector(".active");
 const all = document.querySelector(".all");
-const items = JSON.parse(localStorage.getItem("items")) || []; // if already exists get from storage otherwise create empty array
+const items = JSON.parse(localStorage.getItem("items")) || [];
+
+// Helper function to update local storage and populate the list
+function updateAndPopulateList(items) {
+  localStorage.setItem("items", JSON.stringify(items));
+  populateList(items, itemList);
+}
 
 function addItem(e) {
   e.preventDefault();
-  console.log("inside submit");
-
-  const value = this.querySelector("[name=item]").value;
-  if (value === null || value.match(/^ *$/) !== null) {
+  const value = this.querySelector("[name=item]").value.trim();
+  if (!value) {
     alert("Add item!");
-    //return;
   } else {
     const item = {
       id: Date.now() + Math.floor(Math.random() * 10),
@@ -20,65 +23,50 @@ function addItem(e) {
       done: false,
     };
     items.push(item);
-    populateList(items, itemList);
-
-    localStorage.setItem("items", JSON.stringify(items)); //converting array to JSON string
-    if (items.length) {
-      document.querySelector(".buttons").style.display = "inline";
-    }
+    updateAndPopulateList(items);
+    document.querySelector(".buttons").style.display = "inline";
   }
-
-  this.reset(); // for resetting the input field
-  // console.log("items", items);
+  this.reset();
 }
+
 function populateList(items = [], itemList) {
   itemList.innerHTML = items
-    .map((item, i) => {
-      return `
+    .map((item, i) => `
       <li>
-      <div class="listItem">
-      <input type = "checkbox" data-id =${item.id} value =${item.text}  ${
-        item.done ? "checked" : ""
-      }/>
-      <label  data-id =${item.id} data-index =${i} for "item${i}">${
-        item.text
-      }</label>
-      <button  data-id =${item.id} value =${item.text} >❌</button>
-      </div>
+        <div class="listItem">
+          <input type="checkbox" data-id="${item.id}" value="${item.text}" ${
+            item.done ? "checked" : ""
+          }/>
+          <label data-id="${item.id}" data-index="${i}" for="item${i}">${item.text}</label>
+          <button data-id="${item.id}" value="${item.text}">❌</button>
+        </div>
       </li>
-      `;
-    })
+    `)
     .join("");
 }
 
 function toggleButton(e) {
-  if (e.target.matches("input[type=checkbox]")) {
-    // skip this unless its an input
-    const el = e.target;
-    console.log("inside toggle button checkbox");
-    Index = items.findIndex((obj) => obj.id == el.dataset.id);
-    items[Index].done = !items[Index].done; //whenever checkbox is clicked done property is changed of item.
-    localStorage.setItem("items", JSON.stringify(items));
-    populateList(items, itemList);
-  } else if (e.target.matches("button")) {
-    console.log("inside toggle button delete");
-    const el = e.target;
-    Index = items.findIndex((obj) => obj.id == el.dataset.id);
+  const el = e.target;
+  if (el.matches("input[type=checkbox]")) {
+    const Index = items.findIndex((obj) => obj.id == el.dataset.id);
+    items[Index].done = !items[Index].done;
+    updateAndPopulateList(items);
+  } else if (el.matches("button")) {
+    const Index = items.findIndex((obj) => obj.id == el.dataset.id);
     items.splice(Index, 1);
-    localStorage.setItem("items", JSON.stringify(items));
-    if (items.length == 0) {
+    if (items.length === 0) {
       document.querySelector(".buttons").style.display = "none";
     }
-    populateList(items, itemList);
+    updateAndPopulateList(items);
   }
 }
-function EditButton(e) {
-  if (e.target.matches("label")) {
-    populateList(items, itemList);
-    const el = e.target;
-    Index = items.findIndex((obj) => obj.id == el.dataset.id);
-    value1 = el.innerHTML;
-    const li = this.children[Index];
+
+function editItem(e) {
+  const el = e.target;
+  if (el.matches("label")) {
+    const Index = items.findIndex((obj) => obj.id == el.dataset.id);
+    const value1 = el.textContent;
+    const li = el.closest("li");
 
     li.classList.add("editing");
     const input = document.createElement("input");
@@ -90,55 +78,42 @@ function EditButton(e) {
     input.select();
     input.addEventListener("keypress", (event) => {
       if (event.key === "Enter") {
-        console.log("inside  enter event");
-        let value2 = input.value;
+        const value2 = input.value;
         items[Index].text = value2;
         li.removeChild(input);
         li.classList.remove("editing");
-        localStorage.setItem("items", JSON.stringify(items));
-        populateList(items, itemList);
+        updateAndPopulateList(items);
       }
     });
   }
 }
+
+function filterItems(predicate) {
+  const filtered = items.filter(predicate);
+  populateList(filtered, itemList);
+}
+
 function checked() {
-  console.log("inside complted");
-  const filtered = items.filter((item) => {
-    if (item.done) {
-      console.log("text", item);
-      return true;
-    }
-  });
-  console.log("tex", filtered);
-  populateList(filtered, itemList);
+  filterItems((item) => item.done);
 }
+
 function unChecked() {
-  console.log("inside unchecked");
-
-  const filtered = items.filter((item) => {
-    if (!item.done) {
-      return true;
-    }
-  });
-  populateList(filtered, itemList);
+  filterItems((item) => !item.done);
 }
-function All() {
-  console.log("inside All");
-  const filtered = items.forEach((element) => {
-    console.log(items);
-  });
 
+function All() {
   populateList(items, itemList);
 }
+
 complt.addEventListener("click", checked);
 active.addEventListener("click", unChecked);
 all.addEventListener("click", All);
 
 formElt.addEventListener("submit", addItem);
 itemList.addEventListener("click", toggleButton);
-itemList.addEventListener("dblclick", EditButton);
-populateList(items, itemList); //populate on load
+itemList.addEventListener("dblclick", editItem);
+
+populateList(items, itemList);
 if (items.length) {
   document.querySelector(".buttons").style.display = "inline";
-  console.log("on load", items);
 }
